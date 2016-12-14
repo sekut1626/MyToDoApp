cb5d64bb308ac6046d412b0a000722ca95728e04
Sebastian <mobile.sekut@gmail.com>
2016-12-14 18:55:38 +0100

    dodanie wykresu do ekranu statystyk

--- app/src/main/java/com/example/sebastian/mytodoapp/StatisticActivity.java
+++ app/src/main/java/com/example/sebastian/mytodoapp/StatisticActivity.java
@@ -1,26 +1,45 @@
 package com.example.sebastian.mytodoapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
 
 public class StatisticActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+
+
+//    private  float[] yData = {};
+    private String[] xData = {"Ilość dodanych zadań", "Ilość aktualnyh zadań", "Ilość usuniętych zadań",
+            "Ilość dodanych notatek", "Ilość usuniętych notatek", " "};
+    PieChart pieChart;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
+
+
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -33,6 +52,79 @@ public class StatisticActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        pieChart = (PieChart) findViewById(R.id.wykres);
+
+        pieChart.setRotationEnabled(true);
+        pieChart.setHoleRadius(25);
+        pieChart.setTransparentCircleAlpha(0);
+        pieChart.setCenterText("dupa");
+        pieChart.setCenterTextSize(15);
+        pieChart.setDrawEntryLabels(true);
+
+        addDataSet();
+
+    }
+
+
+    private void addDataSet() {
+        TextView sta1 = (TextView) findViewById(R.id.stat1);
+        String s1 = sta1.getText().toString();
+        int S1 = Integer.parseInt(s1);
+
+        TextView sta2 = (TextView) findViewById(R.id.stat2);
+        String s2 = sta2.getText().toString();
+        int S2 = Integer.parseInt(s2);
+
+        TextView sta3 = (TextView) findViewById(R.id.stat3);
+        String s3 = sta3.getText().toString();
+        int S3 = Integer.parseInt(s3);
+
+        TextView sta4 = (TextView) findViewById(R.id.stat4);
+        String s4 = sta4.getText().toString();
+        int S4 = Integer.parseInt(s4);
+
+        TextView sta5 = (TextView) findViewById(R.id.stat5);
+        String s5 = sta5.getText().toString();
+        int S5 = Integer.parseInt(s5);
+
+
+        ArrayList<PieEntry> yEntries = new ArrayList<>();
+        ArrayList<String> xEntries = new ArrayList<>();
+
+
+        yEntries.add(new PieEntry(S1));
+        yEntries.add(new PieEntry(S2));
+        yEntries.add(new PieEntry(S3));
+        yEntries.add(new PieEntry(S4));
+        yEntries.add(new PieEntry(S5));
+
+//        for(int i=0; i<yData.length; i++){
+//            yEntries.add(new PieEntry(yData[i], i));
+//        }
+
+        for(int i=0; i<xData.length; i++){
+            xEntries.add(xData[i]);
+        }
+
+        PieDataSet pieDataSet = new PieDataSet(yEntries, "statystyki");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+
+        ArrayList<Integer> colors = new ArrayList<>();;
+        colors.add(Color.BLUE);
+        colors.add(Color.CYAN);
+        colors.add(Color.GRAY);
+        colors.add(Color.GREEN);
+        colors.add(Color.MAGENTA);
+
+        pieDataSet.setColors(colors);
+
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
+
     }
 
     @Override
